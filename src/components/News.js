62e25b3e487b3b2a01d58461d24fff36d8a8d226
Syed Suleman <sydsulmn7@gmail.com>
2022-01-20 22:28:26 +0530

    added card to display newsitem

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,31 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-2">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row my-3">
+          <div className="col-md-4">
+            <NewsItem
+              title="cricket"
+              description="Some quick example text to build on the card title and make up the
+            bulk of the card's content."
+            />
+          </div>
+          <div className="col-md-4 ">
+            <NewsItem
+              title="cricket"
+              description="Some quick example text to build on the card title and make up the
+            bulk of the card's content."
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="cricket"
+              description="Some quick example text to build on the card title and make up the
+            bulk of the card's content."
+            />
+          </div>
+        </div>
       </div>
     );
   }
